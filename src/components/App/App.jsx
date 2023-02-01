5ca2dd8c15c2747e7e63ef865b4daa25cd3d63c1
SvitlanaChudak <svit.lemon@gmail.com>
2023-02-01 16:03:44 +0200

    Add SharedLayout

---
+++ src/components/App/App.jsx
@@ -0,0 +1,28 @@
+import { Routes, Route } from "react-router-dom";
+import { SharedLayout } from "components/SharedLayout/SharedLayout";
+import { Home } from "../../pages/Home/Home";
+import { Movies } from '../../pages/Movies/Movies';
+import { MovieDetails } from '../../pages/MovieDetails/MovieDetails';
+import { Cast } from '../../pages/Cast/Cast';
+import { Reviews } from '../../pages/Reviews/Reviews';
+import { NotFound } from '../../pages/NotFound/NotFound';
+
+export const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/movies/:movieId" element={<MovieDetails/>}>
+            <Route path="cast" element={<Cast/>}/>
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          </Route>
+        <Route path="*" element={<NotFound />} />
+</Routes>
+    </>
+  );
+};
+
+
